ccf8663981572dd6f560843a1c40b1e88a28feb5
Th3D4rKnight <raja_raghav@hotmail.com>
2018-01-14 10:33:15 +0530

    first

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var path = require("path");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
