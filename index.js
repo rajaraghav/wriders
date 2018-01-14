aa03015a0c145b928d7b186915ae2900f83575d0
Th3D4rKnight <raja_raghav@hotmail.com>
2018-01-14 10:22:40 +0530

    upload

---
+++ index.js
@@ -0,0 +1,11 @@
+express = require("express");
+var path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(4000);
